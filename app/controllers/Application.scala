1c938e899fdae044b6211f35d53a959556d99432
fuji-ta <ringoika@fuji-ta.com>
2014-04-29 16:25:07 +0900

    add navigation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,31 +5,14 @@ import play.api.mvc._
 import play.api.db._
 import play.api.data._
 import play.api.data.Forms._
-import models.Task
 import models.Corp_acc_smry
 import models.Corp_bss
 import scala.collection.mutable.ListBuffer
 
 object Application extends Controller {
 
-  def index = Action {
-    Redirect(routes.Application.tasks)
-  }
-  def tasks = Action {
-    Ok(views.html.index(Task.all(), taskForm))
-  }
-
-  def newTask = Action { implicit request =>
-    taskForm.bindFromRequest.fold(
-      errors => BadRequest(views.html.index(Task.all(), errors)),
-      label => {
-        Task.create(label)
-        Redirect(routes.Application.tasks)
-      })
-  }
-  def deleteTask(id: Long) = Action {
-    Task.delete(id)
-    Redirect(routes.Application.tasks)
+  def index = Action{
+    Ok(views.html.index())
   }
 
   def nri = Action {
@@ -52,7 +35,4 @@ object Application extends Controller {
     }
     Ok(views.html.wvb(buffer.toList, countryis))
   }
-
-  val taskForm = Form(
-    "label" -> nonEmptyText)
 }
