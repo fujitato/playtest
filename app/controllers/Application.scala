1868447b0f866a9c07876fdee908b9f16ff6de76
fuji-ta <ringoika@fuji-ta.com>
2014-04-29 18:03:35 +0900

    add tej and nie

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,38 +11,93 @@ import scala.collection.mutable.ListBuffer
 
 object Application extends Controller {
 
-  def index = Action{
+  def index = Action {
     Ok(views.html.index())
   }
 
   def nri = Action {
-    val countryis: List[String] = Corp_bss.countrylistBySplrId(1)
+    val countryis: List[String] = Corp_bss.countrylistBySplrId(Nri.splr_id)
 
     val buffer = ListBuffer[Tuple2[String, List[Corp_acc_smry]]]()
     for (i: String <- countryis) {
-      val global_ids: List[String] = Corp_bss.globalIdsBySplrRegion(i, 1).map(p => p.global_id)
+      val global_ids: List[String] = Corp_bss.globalIdsBySplrRegion(i, Nri.splr_id).map(p => p.global_id)
       if (global_ids.size > 0) {
         val ret: List[Corp_acc_smry] = Corp_acc_smry.allPeriodBySplrRegionFY2011On(global_ids)
         val t = (i, ret)
         buffer.+=(t)
       }
     }
-    Ok(views.html.nri(buffer.toList, countryis))
+    Ok(views.html.singleCoverage(buffer.toList, countryis, Nri.supplierName))
 
   }
 
   def wvb = Action {
-    val countryis: List[String] = Corp_bss.countrylistBySplrId(9)
+    val countryis: List[String] = Corp_bss.countrylistBySplrId(Wvb.splr_id)
 
     val buffer = ListBuffer[Tuple2[String, List[Corp_acc_smry]]]()
     for (i: String <- countryis) {
-      val global_ids: List[String] = Corp_bss.globalIdsBySplrRegion(i, 9).map(p => p.global_id)
+      val global_ids: List[String] = Corp_bss.globalIdsBySplrRegion(i, Wvb.splr_id).map(p => p.global_id)
       if (global_ids.size > 0) {
         val ret: List[Corp_acc_smry] = Corp_acc_smry.allPeriodBySplrRegionFY2011On(global_ids)
         val t = (i, ret)
         buffer.+=(t)
       }
     }
-    Ok(views.html.wvb(buffer.toList, countryis))
+    Ok(views.html.multiCoverage(buffer.toList, countryis, Wvb.supplierName))
   }
+
+  def nice = Action {
+    val countryis: List[String] = Corp_bss.countrylistBySplrId(Nice.splr_id)
+
+    val buffer = ListBuffer[Tuple2[String, List[Corp_acc_smry]]]()
+    for (i: String <- countryis) {
+      val global_ids: List[String] = Corp_bss.globalIdsBySplrRegion(i, Nice.splr_id).map(p => p.global_id)
+      if (global_ids.size > 0) {
+        val ret: List[Corp_acc_smry] = Corp_acc_smry.allPeriodBySplrRegionFY2011On(global_ids)
+        val t = (i, ret)
+        buffer.+=(t)
+      }
+    }
+    Ok(views.html.singleCoverage(buffer.toList, countryis, Nice.supplierName))
+  }
+  def tej = Action {
+    val countryis: List[String] = Corp_bss.countrylistBySplrId(Tej.splr_id)
+
+    val buffer = ListBuffer[Tuple2[String, List[Corp_acc_smry]]]()
+    for (i: String <- countryis) {
+      val global_ids: List[String] = Corp_bss.globalIdsBySplrRegion(i, Tej.splr_id).map(p => p.global_id)
+      if (global_ids.size > 0) {
+        val ret: List[Corp_acc_smry] = Corp_acc_smry.allPeriodBySplrRegionFY2011On(global_ids)
+        val t = (i, ret)
+        buffer.+=(t)
+      }
+    }
+    Ok(views.html.multiCoverage(buffer.toList, countryis, Tej.supplierName))
+  }
+}
+
+trait supplier {
+  val supplierName: String
+  val splr_id: Integer
+
+}
+
+case object Nice extends supplier {
+  val supplierName: String = "NICE"
+  val splr_id: Integer = 3
+}
+
+case object Nri extends supplier {
+  val supplierName: String = "NRI"
+  val splr_id: Integer = 1
+}
+
+case object Wvb extends supplier {
+  val supplierName: String = "Wvb"
+  val splr_id: Integer = 9
+}
+
+case object Tej extends supplier {
+  val supplierName: String = "Tej"
+  val splr_id: Integer = 4
 }
